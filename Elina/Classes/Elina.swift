e07be7736e633961e1498e72d365d5657f3d1d41
Unknown <ffasang123@icloud.com>
2018-04-24 17:23:56 +0900

    엘리나냥 기여워~

--- Elina/Classes/Elina.swift
+++ Elina/Classes/Elina.swift
@@ -8,87 +8,137 @@
 
 import Foundation
 
+var _ELINA_MAXIMUM_DRINK_AMOUNT_: Double {
+    return 1
+}
+
+enum ELState{
+    case normal
+    case gaming
+    case drunken
+}
+
+
+
 class Elina {
-    enum State{
-        case normal
-        case drunken
+    
+    enum Sns: String{
+        case twitter = "Twitter"
+        case facebook = "Facebook"
     }
     
-    enum Sns{
-        case twitter
-        case facebook
+    
+    
+    struct Posts{
+        var postIndex: String
     }
     
-    enum Topic{
-        case rhythmGame
-        case drunken
-    }
-    
-    let drunkenScripts: [String] = [
-        "여러분 엘리나쨩귀엽죠???? 저도그렇게생각해요 세상에서제일귀여워 깨물어주고시퍼 꺄아앙♥",
-        "귀여운 엘리나쨩이 간만에 술마셔쪄!! 헤헤헤 칭찬해줘어어 호메뗴 호메떼!!! ♡",
-        "ㅈ진짜 미치겠네 이걸 마시고 취하는새럼이 어딨어 나진짜 아 ㅠㅠ 어디가서 술마신다고 하면 안되겟따...",
-        "안녀엉ㅇ 엘리나쨩 또 술마셨어요!! 헤헿 어떻게 막걸리 한병 1.2리터를 마시고 기분이 팍 좋아져버리고 걷는길제대로못걷냐 미치겠네 진짜ㅋㅋㅋㅋㅋㅋㅋㅋㅋ",
-        "죄송해요 이런 소란을 다피우고ㅠㅠㅠㅠ 그냥 발닦고 잘께요 엘리나냥ㅇ 우유먹고 자자아ㅏ!!",
-        "기여운 엘리나냥 데려가서 키우실분!!! 분양비는 무료다냐아아앙 헤ㅔ헤 ♡",
-        "냐아어앙 엘리나냥으로 바꿔버려야겠다냐아아!!!",
-        "냥냥이귀엽져!!!! 엘리나냥도 짱귀여워여여ㅕㅓ어엉어ㅓ💕💕💕",
-        "냐아아앙 엘리나냥이 모두를 지켜줄꺼라냐아아아!!!",
-        "댕커여운 엘리나냥은 진짜루자러갈께냐아아아💕💕",
-        "솔직히님들도 엘리나냥 기대하잖아요 내가 되어주면되는거지!!!!!!!"
-    ]
-    
-    let rhythmScripts: [String] = [
-        
-    ]
+    var twitterPosts: [Posts] = []
+    var facebookPosts: [Posts] = []
     
     var alcohol: Double
-    var state: State
+    fileprivate var state: ELState
+    
+    
     
     init() {
         self.alcohol = 0
         self.state = .normal
     }
     
-    func drink(amount alcohol: Double) -> Void {
-        self.alcohol = self.alcohol + alcohol
+    init(drink amount: Double) {
+        if amount >= _ELINA_MAXIMUM_DRINK_AMOUNT_ {
+            self.alcohol = _ELINA_MAXIMUM_DRINK_AMOUNT_
+            self.state = .drunken
+        }else{
+            if amount < 0 {
+                self.alcohol = 0
+            }else{
+                
+                self.alcohol = amount
+            }
+            self.state = .normal
+        }
+    }
+    
+    
+    func drink(_ amount: Double){
+        if amount < 0 {
+            self.alcohol = 0
+        }
+        else if amount >= _ELINA_MAXIMUM_DRINK_AMOUNT_{
+            self.alcohol = _ELINA_MAXIMUM_DRINK_AMOUNT_
+            self.state = .drunken
+        }
+        else{
+            self.alcohol = amount
+        }
+    }
+    
+    func startGame(){
+        self.state = .gaming
+    }
+    
+    func stopGame(){
+        if alcohol == _ELINA_MAXIMUM_DRINK_AMOUNT_{
+            state = .drunken
+        }
+        else{
+            state = .normal
+        }
+    }
+    
+    func postSomething(){
         
-        if self.alcohol >= 200{
-            feel(like: .drunken)
+        var indexString: String{
+            switch state{
+            case .normal:
+                return ELScripts.general[Int(arc4random_uniform(UInt32(Int32(ELScripts.general.count))))]
+            case .gaming:
+                return ELScripts.rhythmGame[Int(arc4random_uniform(UInt32(ELScripts.rhythmGame.count)))]
+            case .drunken:
+                return ELScripts.drunken[Int(arc4random_uniform(UInt32(ELScripts.drunken.count)))]
+            }
         }
-        
-    }
-    
-    func feel(like state: State) -> Void {
-        switch state {
-        case .normal:
-            write(at: .facebook, about: .rhythmGame)
-            write(at: .twitter, about: .rhythmGame)
-        case .drunken:
-            write(at: .twitter, about: .drunken)
-        }
-    }
-    
-    func write(at sns: Sns, about topic: Topic)-> Void{
-        switch topic {
-        case .rhythmGame:
-            post(at: sns, "19렙 PUC!!!!!!!!!!")
-        case .drunken:
-            randomPost(at: sns, with: drunkenScripts)
-        }
-    }
-    
-    private func randomPost(at sns:Sns, with scripts: [String]){
-        post(at: sns, scripts[Int(arc4random_uniform(UInt32(scripts.count)))])
-    }
-    
-    private func post(at sns:Sns, _ context: String){
-        switch sns {
-        case .facebook:
-            print("Elina님이 Facebook에 업로드 : \(context)")
+        switch chooseSnsByState() {
         case .twitter:
-            print("Elina님이 Twitter에 트윗 : \(context)")
+            twitterPosts.append(Posts(postIndex: indexString))
+        case .facebook:
+            facebookPosts.append(Posts(postIndex: indexString))
         }
+    }
+    
+    private func chooseSnsByState()-> Sns{
+        var avaliabilityOfChoosingFacebook: Int{
+            switch self.state {
+            case .normal:
+                return 46
+            case .gaming:
+                return 27
+            case .drunken:
+                return 3
+            }
+        }
+        
+        if arc4random_uniform(100) > avaliabilityOfChoosingFacebook{
+            return .twitter
+        }else{
+            return .facebook
+        }
+    }
+    
+    
+    
+    private func postScript(at sns:Sns, script: [String])-> String{
+        var uploadProfileAndTypeBySns:(String, String){
+            switch sns {
+            case .facebook:
+                return ("김**", "업로드")
+            case .twitter:
+                return ("Elina", "트윗")
+            }
+        }
+        return "\(uploadProfileAndTypeBySns.0)님이 \(sns.rawValue)에 \(uploadProfileAndTypeBySns.1) : \(script[Int(arc4random_uniform(UInt32(script.count)))])"
     }
 }
 
--- README.md
+++ README.md
@@ -5,20 +5,97 @@
 [![License](https://img.shields.io/cocoapods/l/Elina.svg?style=flat)](http://cocoapods.org/pods/Elina)
 [![Platform](https://img.shields.io/cocoapods/p/Elina.svg?style=flat)](http://cocoapods.org/pods/Elina)
 
-## Example
+**이 프로젝트는 암묵의 룰에 따라 모든 커밋의 제목을 __엘리나냥 기여워~__로 통합하는 것을 원칙으로 합니다.**
 
-To run the example project, clone the repo, and run `pod install` from the Example directory first.
+## Overview
+
+엘리나냥 프로젝트는 초 카와이 엘리나냥의 커여움을 세계 모두가 알게하기 위한 프로젝트입니다.
 
 ## Requirements
 
+* ~~엘리나냥의 귀여움에도 심쿵하지 않고 버텨낼 강한 정신력~~ 너무 과도한 귀여움으로 세계의 누구도 해내지 못하여 문단을 삭제
+
+## How to use this?
+
+Elina의 기본적인 기능은 귀여운 Elina의 대박심쿵초카와이짱짱마제스티커여움을 자랑하는 어록들을 둘러볼 수 있도록 해줍니다.
+
+기본적으로, Elina 객체를 생성합니다.
+
+```Swift
+let elina: Elina = Elina()
+
+// 옵션을 통해 엘리나의 상태에 변화를 준 채 생성하는 것도 가능합니다.
+// 예를들면, 이 경우에는 엘리나를 술을 꽤 먹인채로 생성합니다.
+let drunkenElina = Elina(drink: 0.8)
+```
+
+Elina 객체에는 갖가지 영향을 줄 수 있습니다. 이를테면 `drink(amount: )`함수로 귀여운 엘리나냥을 취하게 만들 수도 있습니다. `startgame()`함수로 엘리나냥이 좋아하는 사운드볼텍스를 플레이 하게 해줄 수도 있지요.
+
+Elina에게 가하는 영향은 Elina의 `state`에 영향을 줍니다.
+
+`state`값은, 엘리나가 SNS에 올릴 글을 결정합니다.
+
+다음은 엘리나에게 술을 먹이고 SNS에 글을 올리게 하는 코드입니다.
+
+```Swift
+//우리 귀여운 엘리나냥을 생성합니다.
+let elina: Elina = Elina()
+
+//우리 귀여운 엘리나냥에게 술을 듬뿍 먹여줍니다
+elina.drink(amount: 999999)
+
+//우리 귀여운 엘리나냥이 SNS에 포스트를 합니다
+elina.postSomething()
+```
+
+엘리나냥은 두개의 SNS 계정을 사용합니다 Facebook 과 Twitter 이죠. 상기한 `state` 값은 둘 중 어느 SNS에 글을 올릴 지 또한 결정합니다. 반드시 둘중 하나를 정하나, `state`에 따라 어느 한 쪽을 고를지에 대한 확률이 커지지요.
+
+방금의 예제에선, 엘리나냥이 만취했으므로 높은 확률로 Twitter에 트윗했을 것입니다. 엘리나냥이 각 SNS에 올린 포스트는 다음과 같은 방법으로 확인할 수 있습니다.
+
+```Swift
+print(elina.twitterPosts[0].postIndex)
+```
+
+```
+실행결과
+----------
+
+```
+
+어? 트위터엔 아무것도 올리지 않았어요!
+
+그렇다면... 설마...
+
+```Swift
+print(elina.facebookPosts[0].postIndex)
+```
+
+```
+실행결과
+----------
+여러분 엘리나쨩귀엽죠???? 저도그렇게생각해요 세상에서제일귀여워 깨물어주고시퍼 꺄아앙♥
+```
+
+저런... 너무나 취한 나머지.... 결국.... 페이스북에.....
+
+
+## Where should I use this?
+
+엘리나냥의 귀여움을 모두에게 전파하세요! 
+
+개인 프로젝트를 비롯하여, 사내 프로젝트, 정부/공공기관, 재단, 그 외 등등 에 엘리나냥의 귀여움을 **몰래** 심으세요! 
+
+설령 들켰다 한들 **엘리나냥의 압도적인 귀여움**에 여러분의 파트너, 상사, 대표님, 대통령님, 우주인 조차 여러분에게 책임을 물지 않을 것입니다! 왜냐하면 엘리나냥은 귀엽거든요.
+
 ## Installation
 
-Elina is available through [CocoaPods](http://cocoapods.org). To install
-it, simply add the following line to your Podfile:
+Elina는 [CocoaPods](http://cocoapods.org)를 지원합니다! 당신만의 귀여운 엘리나냥을 만나보세요! 간단히 `Podfile`에 :
 
 ```ruby
 pod 'Elina'
 ```
+
+만 입력하시면 됩니다!
 
 ## Author
 
@@ -26,4 +103,8 @@ Cyanide7523, ffasang123@icloud.com
 
 ## License
 
-Elina is available under the MIT license. See the LICENSE file for more info.
+Elina는 MIT license가 적용되어 있습니다. 라이선스에 관한 정보는 LICENSE 파일에서 확인해주세요.
+
+## Why does '엘리나냥' is kawaii?
+
+왜냐하면 엘리나냥은 귀엽기 때문입니다.
